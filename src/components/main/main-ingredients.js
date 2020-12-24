5af79bbff7d315487296ecee88cd9d2d2206e33d
kimts2627 <kimtstststs@gmail.com>
2020-12-24 15:53:47 +0900

    Add some comments

--- src/components/main/main-ingredients.js
+++ src/components/main/main-ingredients.js
@@ -10,45 +10,46 @@ class MainIngre extends Component {
         ['고추장','gochujang'],['돼지고기','pork'],['치킨','chicken'],['참치','tuna'],
         ['밀가루','flour'],['김치','kimchi'],['누텔라','nutella'],['포카칩','pocachip']
       ],
-      shoppingBag: []
+      shoppingBag: []  /* 재료 클릭 시 쇼핑백에 담기게 되며, 재 클릭시 쇼핑백에서 사라짐 */
     }
     this.selectOrDeleteIngredient = this.selectOrDeleteIngredient.bind(this);
   }
 
+  /* 재료버튼 클릭, 재클릭 핸들링 함수 */
   selectOrDeleteIngredient(e) {
     let target = e.target.textContent;
     console.log(target);
     
-    if(e.target.classList.contains('main-ingredient')) {
-      for(let i of this.state.ingredients) {
+    if(e.target.classList.contains('main-ingredient')) {  /* CSS 스타일 이름을 통해 선택 유/무를 판별하며 */
+      for(let i of this.state.ingredients) {  /* 재료목록과 대조하여 영문이름 수집 후 쇼핑백에 담음 */
         if(i[0] === target) {
           target = i[1];
         }
       }
 
       this.setState({ shoppingBag: [...this.state.shoppingBag, target] });
-      e.target.classList.remove('main-ingredient');
+      e.target.classList.remove('main-ingredient');  /* 재료버튼 스타일을 클릭전 에서 클릭후 상태로 바꿈 */
       e.target.classList.add('main-ingredient-clicked');
     }
     else {
       const {shoppingBag, ingredients} = this.state;
 
-      for(let i of ingredients) {
+      for(let i of ingredients) {  /* 재료목록과 대조하여 영문이름 수집 */
         if(i[0] === target) {
           target = i[1];
         }
       }
 
-      let resultShoppingBag = shoppingBag.slice();
+      let resultShoppingBag = shoppingBag.slice();  /* 쇼핑백 얕은복사 */
 
-      for(let i = 0; i < shoppingBag.length; i ++) {
+      for(let i = 0; i < shoppingBag.length; i ++) {  /* 쇼핑백과 수집한 영문이름 대조하여 해당이름 쇼핑백에서 제거 */
         if(shoppingBag[i] === target) {
           resultShoppingBag.splice(i, 1);
           this.setState({ shoppingBag: resultShoppingBag });
         }
       }
 
-      e.target.classList.remove('main-ingredient-clicked');
+      e.target.classList.remove('main-ingredient-clicked');  /* 재료버튼 스타일을 클릭후 에서 클릭전 상태로 바꿈 */
       e.target.classList.add('main-ingredient');
     }
   }
--- src/components/main/main.js
+++ src/components/main/main.js
@@ -2,24 +2,27 @@ import React, { Component } from 'react'
 import MainHeader from './main-header'
 import MainIngre from './main-ingredients'
 import Chef from './main-chef'
+import axios from 'axios'
 
 class Main extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      isOpenning: true,
-      talkingCount: 0,
-      chefsMessage: null,
-      isLogin: false,
-      isMypageOpen: false,
-      isModalOpen: false,
-      username: null
+      isOpenning: true,  /* 오프닝 씬인지 아닌지 여부 */
+      talkingCount: 0,  /* 오프닝 씬 내에서 셰프가 얘기한 숫자, 해당 숫자에 따라 알맞은 대사를 chefsMessage에 할당. 5가 맥시멈 */
+      chefsMessage: null,  /* 셰프 대사 */
+      isLogin: false,  /* 로그인 여부 판별 */
+      isMypageOpen: false,  /* 마이페이지가 열렸는지 여부 판별 */
+      isModalOpen: false,  /* 모달창이 열렸는지 여부 판별 */
+      username: null  /* 로그인 했을 시 사용자 이름 */
     }
     this.talkingCountUp = this.talkingCountUp.bind(this);
     this.changeMessage = this.changeMessage.bind(this);
     this.handleMypage = this.handleMypage.bind(this);
+    this.getOneMeal = this.getOneMeal.bind(this);
   }
 
+  /* talkingCount state에 할당된 번호에 따라 chefsMessage에 알맞은 대사 삽입 */
   changeMessage() {
     const {talkingCount} = this.state;
     if(talkingCount === 0) {
@@ -39,10 +42,11 @@ class Main extends Component {
     }
   }
 
+  /* 화면 클릭 때 마다 talkingCount state 숫자 올려주기 */
   talkingCountUp() {
-    this.changeMessage();
+    this.changeMessage();  /* 숫자 올려주며 chefsMesaage도 같이 변환 */
     this.setState({ talkingCount: this.state.talkingCount + 1 });
-    if(this.state.talkingCount === 5) {
+    if(this.state.talkingCount === 5) {  /* 숫자가 한계치인 5에 다다르면 더이상 클릭해도 숫자를 올릴 수 없으며 오프닝이 종료됨. */
       this.setState({
         isOpenning: false,
         chefsMessage: '냉장고에 있는 재료들을 골라주세요!'
@@ -50,8 +54,22 @@ class Main extends Component {
     }
   }
 
+  /* 마이페이지 핸들링 클릭이벤트 함수 */
   handleMypage() {
     this.setState({ isMypageOpen: !this.state.isMypageOpen})
+  }
+
+  /* 셰프에게 추천받기 버튼 서버통신 함수 */
+  getOneMeal() {
+    axios({
+      method: 'get',
+      url: 'https://onemeal.site/resultrecipe',
+      headers: { 'Content-Type': 'application/json'},
+      withCredentials: true
+    })
+    .then(res => {
+      console.log(res)
+    })
   }
 
 
@@ -65,13 +83,12 @@ class Main extends Component {
         </span>
         <Chef isOpenning={isOpenning} chefsMessage={chefsMessage} />
         {
-          isOpenning
-          ? null
-          :
-          <div className='main-main'>
+          isOpenning 
+          ? null              /* 오프닝이 참 일때 아래 엘리먼트 및 컴포넌트는 렌더링 하지 않음 */
+          :<div className='main-main'>
             <MainHeader isLogin={isLogin}/>
             <MainIngre />
-            <button className='main-submit'>셰프에게 추천받기</button>
+            <button className='main-submit' onClick={this.getOneMeal}>셰프에게 추천받기</button>
           </div>
         }
         </div>
