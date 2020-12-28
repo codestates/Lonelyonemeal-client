16b273f5c5aa48b4dc285e1c6de7b2a44e85caab
harry1757 <hsw9724@gmail.com>
2020-12-28 10:42:23 +0900

    add result

--- src/components/main/main.js
+++ src/components/main/main.js
@@ -4,6 +4,8 @@ import MainIngre from './main-ingredients'
 import Chef from './main-chef'
 import axios from 'axios'
 import Signin from '../user/signin'
+import Result from '../user/result'
+
 
 class Main extends Component {
   constructor(props) {
@@ -12,24 +14,44 @@ class Main extends Component {
       chefsMessage: null,
       isLogin: false,  /* 로그인 여부 판별 */
       isLoginModalOpen: false,  /* 로그인 모달창이 열렸는지 여부 판별 */
-      username: null  /* 로그인 했을 시 사용자 이름 */
+      username: null  /* 로그인 했을 시 사용자 이름 */,
+      githubToken: ""
     }
     this.getOneMeal = this.getOneMeal.bind(this);
     this.loginHandler = this.loginHandler.bind(this);
     this.loginModalHandler = this.loginModalHandler.bind(this);
+    this.getGithubToken = this.getGithubToken.bind(this)
   }
+
+  async getGithubToken(authorizationCode) {
+    const result = await axios.post("https://onemeal.site/githubLogin", { authorizationCode }, { withCredentials: true })
+    this.setState({
+      isLogin: true,
+      githubToken: result.data.accessToken
+    })
+  }
+
+
+  componentDidMount() {
+    const url = new URL(window.location.href)
+    const authorizationCode = url.searchParams.get('code')
+    if (authorizationCode) {
+      this.getGithubToken(authorizationCode)
+    }
+  }
+
 
   /* 셰프에게 추천받기 버튼 서버통신 함수 */
   getOneMeal() {
     axios({
-      method: 'get',
+      method: 'GET ',
       url: 'https://onemeal.site/',
-      headers: { 'Content-Type': 'application/json'},
+      headers: { 'Content-Type': 'application/json' },
       withCredentials: true
     })
-    .then(res => {
-      console.log(res)
-    })
+      .then(res => {
+        console.log(res)
+      })
   }
 
   /* 로그인 상태 변경 함수 */
@@ -48,16 +70,16 @@ class Main extends Component {
 
 
   render() {
-    const {chefsMessage, isLogin, isLoginModalOpen, username} = this.state;
-    return(
+    const { chefsMessage, isLogin, isLoginModalOpen, username } = this.state;
+    return (
       <div className='main-wrap'>
-        <Chef chefsMessage={chefsMessage} />   
+        <Chef chefsMessage={chefsMessage} />
         <div className='main-main'>
-          <MainHeader isLogin={isLogin} username={username} loginModalHandler={this.loginModalHandler}/>
+          <MainHeader isLogin={isLogin} username={username} loginModalHandler={this.loginModalHandler} />
           <MainIngre />
           <button className='main-submit' onClick={this.getOneMeal}>셰프에게 추천받기</button>
         </div>
-        {isLoginModalOpen ? <Signin loginHandler={this.loginHandler} loginModalHandler={this.loginModalHandler}/> : null}
+        {isLoginModalOpen ? <Signin loginHandler={this.loginHandler} loginModalHandler={this.loginModalHandler} /> : null}
       </div>
     );
   }
--- src/components/user/signin.js
+++ src/components/user/signin.js
@@ -2,10 +2,11 @@ import React, { Component } from 'react'
 import logo from './img2/logo.png'
 import github from './img2/github.png'
 import google from './img2/google.png'
-import {Link , withRouter} from 'react-router-dom'
+import { Link, withRouter } from 'react-router-dom'
 import axios from 'axios'
 import x from './img2/X.png'
 import Signup from './signup'
+import { GoogleLogin } from "react-google-login"
 
 class Signin extends Component {
   constructor(props) {
@@ -13,12 +14,24 @@ class Signin extends Component {
     this.state = {
       email: "",
       password: "",
-      showSignupView: false
+      showSignupView: false,
+      errorMessage: "",
+      googleId: ""
     };
     this.handleInputValue = this.handleInputValue.bind(this);
     this.loginRequestHandler = this.loginRequestHandler.bind(this);
     this.handleSignupView = this.handleSignupView.bind(this);
+    this.socialLoginHandler = this.githubLoginHandler.bind(this);
+
   }
+
+  async githubLoginHandler() {
+    let result = await axios.get("https://onemeal.site/githubLogin", { withCredentials: true })
+    let GITHUB_LOGIN_URL = `https://github.com/login/oauth/authorize?client_id=${result.client_id}`
+    window.location.assign(GITHUB_LOGIN_URL)
+  }
+
+
 
   handleInputValue(e) {
     this.setState({ [e.target.name]: e.target.value });
@@ -27,12 +40,15 @@ class Signin extends Component {
   handleSignupView() {
     this.setState({ showSignupView: !this.state.showSignupView });
   }
-  
-  
+
+
   loginRequestHandler() {
-    const {email, password} = this.state;
-    if(!email || !password) {
-      return; // 입력요청 출력
+    const { email, password } = this.state;
+
+    if (email === "" || password === "") {
+      this.setState({
+        errorMessage: "모든항목을 입력해주세요"
+      })
     }
     else {
       axios({
@@ -42,65 +58,71 @@ class Signin extends Component {
         withCredentials: true,
         data: { email: email, password: password }
       })
-      .then(res => {
-        console.log(res.data);
-        return (
-          axios({
-            method: 'GET',
-            url: 'http://onemeal.site/users/userinfo',
-            headers: { 'Content-Type': 'application/json' },
-            withCredentials: true,
-          })
-        )
-      })
-      .then(res => {
-        console.log(res.data.userInfo.username);
-        this.props.loginHandler(res.data.userInfo.username);
-      })
-      .catch(err => {
-        console.log(err);
-      })
+        .then(res => {
+          console.log(res.data);
+          return (
+            axios({
+              method: 'GET',
+              url: 'https://onemeal.site/users/userinfo',
+              headers: { 'Content-Type': 'application/json' },
+              withCredentials: true,
+            })
+          )
+        })
+        .then(res => {
+          console.log(res.data.userInfo.username);
+          this.props.loginHandler(res.data.userInfo.username);
+        })
+        .catch(err => {
+          console.log(err);
+        })
     }
   }
 
   render() {
+
+    const responseGoogle = (any) => {
+
+    }
     return (
       this.state.showSignupView ?
-      <Signup handleSignupView={this.handleSignupView} loginModalHandler={this.props.loginModalHandler}/> :
-      <div className="back">
-        <div className='loginContainer'>
-          <img className='X' src={x} onClick={this.props.loginModalHandler}/>
-          <div className="middleContainer">
-            <img className="go" src={logo} alt="go" />
-            <div className='block1' />
-            <input
-              className="inputBox"
-              name='email'
-              onChange={(e) => this.handleInputValue(e)}
-              placeholder='이메일'
-              type='text' />
-            <div className='block' />
-            <input
-              className="inputBox"
-              name='password'
-              onChange={(e) => this.handleInputValue(e)}
-              placeholder='비밀번호'
-              type='password' />
-            <div className='block' />
-            <button className="button" onClick={this.loginRequestHandler}>로그인</button>
-            <div className='block' />
-            <span className="text">
-              아직 아이디가 없으신가요?
+        <Signup handleSignupView={this.handleSignupView} loginModalHandler={this.props.loginModalHandler} /> :
+        <div className="back">
+          <div className='loginContainer'>
+            <img className='X' src={x} onClick={this.props.loginModalHandler} />
+            <div className="middleContainer">
+              <img className="go" src={logo} alt="go" />
+              <div className='block1' />
+              <input
+                className="inputBox"
+                name='email'
+                onChange={(e) => this.handleInputValue(e)}
+                placeholder='이메일'
+                type='text' />
+              <div className='block' />
+              <input
+                className="inputBox"
+                name='password'
+                onChange={(e) => this.handleInputValue(e)}
+                placeholder='비밀번호'
+                type='password' />
+              <div className='block' />
+              <button className="button" onClick={this.loginRequestHandler}>로그인</button>
+              <div className='block' />
+              <div className="alert-box">{this.state.errorMessage}</div>
+              <div className='block' />
+              <span className="text">
+                아직 아이디가 없으신가요?
               <span className='text2' onClick={this.handleSignupView}>회원가입</span>
-            </span>
-            <div className='block' />
-            <div>
-              <img className="github" src={github} alt="github" /> <img className="google" src={google} alt="google" />
+              </span>
+              <div className='block' />
+              <div>
+                <img className="github" src={github} alt="github" onClick={this.githubLoginHandler} />  <img className="google" src={google} alt="google" />
+              </div>
             </div>
+            <div className="ex">2021 Baegopujo all rights reserved.</div>
           </div>
-          <div className="ex">2021 Baegopujo all rights reserved.</div>
         </div>
-      </div>
     )
   }
 }
