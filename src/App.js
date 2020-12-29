d3fe59b521c6315c6885c834d8593b4b88df57ad
kimts2627 <kimtstststs@gmail.com>
2020-12-29 15:03:00 +0900

    Edit App.js to Mypage

--- src/App.js
+++ src/App.js
@@ -14,16 +14,19 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-
+      isLogin: false,
       isIntro: true,
       isOpenning: true,
      
     }
     this.handleIntroClicked = this.handleIntroClicked.bind(this);
     this.handleOpenningClicked = this.handleOpenningClicked.bind(this);
+    this.handleLogin = this.handleLogin.bind(this);
   }
 
-
+  handleLogin() {
+    this.setState({ isLogin: !this.state.isLogin });
+  }
 
   handleIntroClicked = () => {
     this.setState({
@@ -40,17 +43,16 @@ class App extends Component {
   }
 
   render() {
-    const {isOpenning, isIntro} = this.state;
+    const {isOpenning, isIntro, isLogin} = this.state;
     return (
       <div className="App" >
         <Switch>
-          <Route exact path='/main' render={() => <Main />} />
+          <Route exact path='/main' render={() => <Main isLogin={isLogin} handleLogin={this.handleLogin}/>} />
           <Route exact path='/intro' render={() => <Intro handleIntroClicked={this.handleIntroClicked} />} />
-          <Route exact path='/signin' render = {() => <Signin/>} />
-          <Route exact path='/signup' render = {() => <Signup/>} />
-          <Route exact path='/openning' render = {() => <Openning isOpenning={isOpenning} handleOpenningClicked={this.handleOpenningClicked}/>} />
-          <Route exact path='/result' render ={()=> <Result/>}/>
-          <Route exact path='/mypage' render ={()=> <Mypage/>}/>
+          <Route exact path='/signin' render = {() => <Signin />} />
+          <Route exact path='/signup' render = {() => <Signup />} />
+          <Route exact path='/openning' render = {() => <Openning isOpenning={isOpenning} handleOpenningClicked={this.handleOpenningClicked} />} />
+          <Route exact path='/mypage' render ={()=> <Mypage isLogin={isLogin} handleLogin={this.handleLogin}/>}/>
           <Route
           path = '/'
           render = {() => {
--- src/components/user/result.js
+++ src/components/user/result.js
@@ -11,7 +11,7 @@ class Result extends Component {
             foodName: "",
             foodImg: ""
         }
-        this.signupRequestHandler = this.signupRequestHandler.bind(this);
+        //this.signupRequestHandler = this.signupRequestHandler.bind(this);
     }
 
   
